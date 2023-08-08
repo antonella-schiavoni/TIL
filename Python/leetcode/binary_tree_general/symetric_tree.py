c42bedde92a615c892c688a5d5d8ce1d6f8116e3
Antonella Schiavoni <antonellaschiavoni2@gmail.com>
2023-08-08 06:49:58 -0300

    feat: add code to check if a binary tree is symetric

---
+++ Python/leetcode/binary_tree_general/symetric_tree.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/symmetric-tree/description/?envType=study-plan-v2&envId=top-interview-150
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+def isMirror(left, right):
+    if not left and not right:
+        return True
+    if not left or not right:
+        return False
+    
+    return left.val == right.val and isMirror(left.left, right.right) and isMirror(left.right, right.left)
+
+def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+    if not root:
+        return True
+    
+    return isMirror(root.left, root.right)
